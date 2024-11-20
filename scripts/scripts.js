3066a869e0991714f3f873611cd44a8512214809
Casey Jones <caseysemail52@gmail.com>
2024-11-19 21:44:18 -0700

    JS Cards done
    Finished up the JS cards

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,15 @@
+/* Template for member entry
+    {
+    header: ,
+    pfp: ,
+    name: ,
+    role: ,
+    bio: ,
+    socials: []
+    },
+*/
+
+
 const member_list = [
     {
         header: "https://yt3.googleusercontent.com/EYhpaxjrgwLo5bwWq80LifICs_MYs5rF8Lm4JdVLK8SrpoeDGH5Axzmaw4Hcj1S0zSkdCOHiiw=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj",
@@ -12,7 +24,120 @@ const member_list = [
         pfp: "https://yt3.googleusercontent.com/JTQb7xKFRPLFXvytzxpbSlk22aDJvaSeD_iwid8_8jz-M6_5vTaHH6FlN04iM6-PRjXW4z9m=s160-c-k-c0x00ffffff-no-rj",
         name: "BlondSkunk",
         role: "Master Architect",
-        bio: "Welcome to the Blondskunk YouTube channel! On this channel, I make videos mostly around the topic of building in Minecraft."
+        bio: "Welcome to the Blondskunk YouTube channel! On this channel, I make videos mostly around the topic of building in Minecraft.",
+        socials: ["https://www.youtube.com/c/blondskunk", "https://www.instagram.com/blondskunk/", "https://www.twitch.tv/blondskunk"]
+    },
+    {
+        header: "https://yt3.googleusercontent.com/bhui7stbZp7H4SgWzMZPbYt1l-8qVK4aETkTukndaGpsVOwfGVcYXZy6VP1NrfGZkWazKEnB=w1138-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj",
+        pfp: "https://yt3.googleusercontent.com/wDRjqeNZEcMGPBBctWeYWSrmO0TDB9Rl9z3i5uZruaDquZTajSZ4cksa1kclxpo4KuERHJtUFQ=s160-c-k-c0x00ffffff-no-rj",
+        name: "Ash's Den",
+        role: "AfterLife Member",
+        bio: "I aim to tell immersive and fun stories in this wonderful game called Minecraft",
+        socials: ["https://www.youtube.com/channel/UCTd1a_UZrBjf88GXdPnNtKg", "https://www.instagram.com/ashsden/", "https://www.twitch.tv/ashsden"]
+    },
+    {
+        header: "https://yt3.googleusercontent.com/uD2KQJg-cQCkcdmL500wxa8lf2SZOPV7BMTU3M8TlHXvFiCcjqxOTgOUofE2bdQK2H3Ba_0n6A=w1138-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj",
+        pfp: "https://yt3.googleusercontent.com/dQjIezu3YnJXwppnNyu5fJ831HUzG7HhrF23Fe96ulzLzPqnXVdMi8gIhLPkbg5Mz0e03nx3=s160-c-k-c0x00ffffff-no-rj",
+        name: "BlockerLocker/Quentin",
+        role: "AfterLife Member",
+        bio: "I play Minecraft and make custom items, minigames, and mistakes.",
+        socials: ["https://www.youtube.com/BlockerLockerYT", "https://www.youtube.com/BlockerLockerYT"]
+    },
+    {
+        header: "https://yt3.googleusercontent.com/VYw3CNMNn4JRwQM_7VDqbkDCgxLan1PnQ7OYy5x8qvGoCNYgxgG2_RZdabirR_9UQudwcw-aEw=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj",
+        pfp: "https://yt3.googleusercontent.com/6iQeshGnVfzT9VCFtwCrd9bImqc_BSncNQg_UnO0k0rC4Dan-KtpMPKiPSKfwfpkeLaNVhinzxY=s160-c-k-c0x00ffffff-no-rj",
+        name: "Fungusaurus Rex",
+        role: "AfterLife Member",
+        bio: "Ready for some awesome Minecraft Builds and adventures? If so then you have come to the right place.",
+        socials: ["https://www.youtube.com/@fungusaurus_rex","https://instagram.com/fungusaurus_rex"]
+    },
+    {
+        header: "https://yt3.googleusercontent.com/0IpvKpIkdISb-pzl6dsVFqY9SzNudr3jaye2c0YFNDTAQrKtyS1arQoWSA4YHbZ5KdwvDdd733g=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj",
+        pfp: "https://yt3.googleusercontent.com/rXfePtcJppJYHSLwlojTFLe6X9KVFKTHbxdG62k3xgnoyUs82xCe1gSSyIJv6DDWNAW1d5TNCw=s160-c-k-c0x00ffffff-no-rj",
+        name: "ImGP",
+        role: "AfterLife Member",
+        bio: "I play Minecraft. I make videos while doing it.",
+        socials: ["https://www.youtube.com/@Im_GP/featured","https://x.com/imgp_yt","https://www.instagram.com/imgp_yt/"]
+    },
+    {
+        header: "https://yt3.googleusercontent.com/_gVLMnSNVScw_Zbzif7v04M-wxZQe729bF1g7eFcZq0B0vhZ4zyjZGXNIprzHo2_Yv6eCue-hQ=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj",
+        pfp: "https://yt3.googleusercontent.com/ytc/AIdro_mSx_TILnUyuHr9Ba4QjYB_bxsOU7zGuhUmFzyEjuQ3CQ=s160-c-k-c0x00ffffff-no-rj",
+        name: "JustinL",
+        role: "AfterLife Member",
+        bio: "Welcome! I'm JustinL and I do gaming commentaries. Play a ton of Minecraft! and other games on rare occasions.",
+        socials: ["https://www.youtube.com/@JustinLYT","https://www.twitter.com/JustinRL94","https://twitch.tv/justinl94"]
+    },
+    {
+        header: "https://yt3.googleusercontent.com/1Hcsm-qzYuYU3vCiUkKn0yYet1zE_5JEKaLKwfIdsSJqBN2vuuOpnhaJm9O0OBw0UwaLiLODvA=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj",
+        pfp: "https://yt3.googleusercontent.com/HTqPGCVU9WQW6AXn7Qct1cnbIADKf-EXhYq8CAJb2HbchbVIjB4GXD_vDRAwgc2lO7RtLdWsmj8=s160-c-k-c0x00ffffff-no-rj",
+        name: "KB's Sandbox",
+        role: "AfterLife Member",
+        bio: "Welcome to KB's Sandbox, where I play Minecraft in all it's variations!",
+        socials: ["https://www.youtube.com/@kbssandbox","https://www.instagram.com/kbssandbox","https://www.twitch.tv/kbssandbox"]
+    },
+    {
+        header: "https://yt3.googleusercontent.com/1y8Bp3tWylCdu-ZzXFzTAbW3Qzsh68g7bMDlU0cP6eCoKJ7junvGJ-SOCz5E-WdPjv5Ies8WZw=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj",
+        pfp: "https://yt3.googleusercontent.com/ZDkHbiieXrlmhcg47A3XgqZzT16raIMQVZGIytmoyXS6Tl2TSY9ythOQddq1yxaU_DTJY7CNVw=s160-c-k-c0x00ffffff-no-rj",
+        name: "KingZee",
+        role: "AfterLife Member",
+        bio: "",
+        socials: ["https://www.youtube.com/@KingZee","https://twitter.afterlifesmp.com","https://instagram.afterlifesmp.com","https://www.twitch.tv/kingzee"]
+    },
+    {
+        header: "https://yt3.googleusercontent.com/QboXfXqpABhClzAJfEJ6wdODJiAMvZspVwzG_tq5Bp_ozEQP-pHaEErs8wVHyD924Deh4a1h=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj",
+        pfp: "https://yt3.googleusercontent.com/83Jaov4YFbEpEslLaojINaGDIm6TjwU-XZNS5S5ikFtwy45kYyUnnh2g7gTeEzT8QTrnYntO3Q=s160-c-k-c0x00ffffff-no-rj",
+        name: "MineyLo",
+        role: "AfterLife Member",
+        bio: "I'm an iPad Minecraft pro, watch me be a Java noob",
+        socials: ["https://www.youtube.com/@mineyLO","https://twitch.tv/mineylo"]
+    },
+    {
+        header: "https://yt3.googleusercontent.com/V48-CYBC1SsZ-bUv-h9vcuVYdF98PipW7rf_yF_k4Xs7mR7YbArO8IqhyWa0LLHtctZuDzLjyA=w1138-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj",
+        pfp: "https://yt3.googleusercontent.com/UmtcucW7BwK73o75axv3A_pphpztda-O2bVNslmIlO5AvjFqmD9yijhwxzjwd5hrhVwSAwxHzQ=s160-c-k-c0x00ffffff-no-rj",
+        name: "PuffingFish HQ",
+        role: "Master Redstoner",
+        bio: "Minecraft player who likes to challenge the typical! Always trying to do stuff in a unique way, hope you enjoy!",
+        socials: ["https://www.youtube.com/@PuffingFish"]
+    },
+    {
+        header: "https://yt3.googleusercontent.com/vYOiEGRVzup0qET4ajImkpMaWz5z-Ic8hXtg7m-lfkrT0Uj89pJd0n7T2L4Rf435C59_08wP=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj",
+        pfp: "https://yt3.googleusercontent.com/ytc/AIdro_nYPzA6-a5kVsuK7_HjhqLiNYpc0hCrgSu9PhkeyzxUOlg=s160-c-k-c0x00ffffff-no-rj",
+        name: "SquareMario",
+        role: "Lead Community Social Chair Executive Director",
+        bio: "Hey there :) I'm a gaming YouTuber that uploads a variety of Minecraft content, but mostly survival and building.",
+        socials: ["https://www.youtube.com/@SquareMario","https://www.twitter.com/SquareMario","https://instagram.com/squaremario_minecraft","https://twitch.tv/squaremario"]
+    },
+    {
+        header: "https://yt3.googleusercontent.com/mRru3I-cUYNxilQdSiB-Bz_z-0rEmZtQTlTrCmZm0FHfHcvtbR3iBIRPh3K5xh1K44OF20Gx=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj",
+        pfp: "https://yt3.googleusercontent.com/oA9Ns8L6qs2Wom_4Gihe9XQXFip45YnDrVctof91ol39v2x8IHzf-oVk_wDr0mpk2FXXT3yQuw=s160-c-k-c0x00ffffff-no-rj",
+        name: "tinkfro",
+        role: "AfterLife Member",
+        bio: "I publish great videos about this wonderful game we have all come to love",
+        socials: ["https://www.youtube.com/@tinkfro"]
+    },
+    {
+        header: "https://yt3.googleusercontent.com/nr3JVPO8elD_gM3di07RzRPAIfJScEI6MNcLkA8-Xy34B4HubDnQSUmH4c79SjxIbeD8R9Q2byg=w1138-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj",
+        pfp: "https://yt3.googleusercontent.com/qpJXJYwHfVxpV0SRTFUqnNNgA9ffrWjPpBwidtPzmT-vBNCKdHrp1ybYnU4IvMpqq-jya3gcKAs=s160-c-k-c0x00ffffff-no-rj",
+        name: "Uni",
+        role: "AfterLife Member",
+        bio: "Timelapses, Let's Plays, Streams, and Rube Goldberg Machines! That and much on here on Uni's Minecraft Channel",
+        socials: ["https://www.youtube.com/@Uni_"]
+    },
+    {
+        header: "https://yt3.googleusercontent.com/2SPjEzm0BP1ETDgDdo5Ij6aVExC4IAoeKo7i37rqXVq1k0UQXs9GvdUehj1fSNFTC478zQ9mqg=w1707-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj",
+        pfp: "https://yt3.googleusercontent.com/ytc/AIdro_ncfMFIidRCAgGU2krCtQyq9ht7OMf5XMavvYVu2XYOzw=s160-c-k-c0x00ffffff-no-rj",
+        name: "WhiteFyr",
+        role: "Fyr Lord",
+        bio: "Minecraft YouTuber with a focus on survival. Here you'll find a lot of vanilla Minecraft, including my hardcore amplified singleplayer series known as Survival Heights!",
+        socials: ["https://www.youtube.com/@WhiteFyr","https://www.x.com/WhiteFyr_","https://instagram.com/whitefyr","https://twitch.tv/WhiteFyr"]
+    },
+    {
+        header: "https://yt3.googleusercontent.com/1j5FRI7QjJ7cfBDWSylYlOO3u7Xen1qz2ft1FtPIPDqQl4VxpbNQAejLkLtmUCES5d5LS9QtBQ=w1138-fcrop64=1,00005a57ffffa5a8-k-c0xffffffff-no-nd-rj",
+        pfp: "https://yt3.googleusercontent.com/AltV9j6t2kt_sqh-0uGj_RlIu52a-ALjAt2IchLJt9X4bFc8X4F3kqFGWG2nk4All_GDeXvz3cM=s160-c-k-c0x00ffffff-no-rj",
+        name: "ztVypr",
+        role: "AfterLife Member",
+        bio: "Working hard to survive in Hardcore Minecraft (and having fun along the way)!",
+        socials: ["https://www.youtube.com/@ztVypr","https://instagram.com/ztvypr"]
     }
 ]
 
@@ -42,22 +167,41 @@ $(document).ready(function () {
 
         // Create The Footer
         var footer_div = document.createElement('div')
-        $(footer_div).addClass("card-footer text-center")
-        for (let j = 0; j < member_list[i].socials.length; j++) {
+        $(footer_div).addClass("row container card-footer text-center m-auto")
+
+        for (var j = 0; j < member_list[i].socials.length; j++) {
             if (member_list[i].socials[j].includes("youtube")) {
                 var social_div = document.createElement('div');
+                $(social_div).addClass("col")
                 var current_social = '<a class="bio-youtube p-1" href= ' + member_list[i].socials[j] + ' target="_blank"><svg class="bi" width="24" height="24"> <use xlink:href="#youtube" /> </svg></a>'
 
                 social_div.innerHTML = current_social;
-                card_div.append(social_div)
-            } else if (member_list[i].socials[i].includes(".x.")) {
+                footer_div.append(social_div)
+            } else if (member_list[i].socials[j].includes(".x.")) {
+                var social_div = document.createElement('div');
+                $(social_div).addClass("col")
+                var current_social = '<a class="p-1" href= ' + member_list[i].socials[j] + ' target="_blank"><svg class="bi" width="24" height="24"> <use xlink:href="#twitter" /> </svg></a>'
 
-            } else if (member_list[i].socials[i].includes("instagram")) {
+                social_div.innerHTML = current_social;
+                footer_div.append(social_div)
+            } else if (member_list[i].socials[j].includes("instagram")) {
+                var social_div = document.createElement('div');
+                $(social_div).addClass("col")
+                var current_social = '<a class="bio-instagram p-1" href= ' + member_list[i].socials[j] + ' target="_blank"><svg class="bi" width="24" height="24"> <use xlink:href="#instagram" /> </svg></a>'
 
-            } else if (member_list[i].socials[i].includes("twitch")) {
+                social_div.innerHTML = current_social;
+                footer_div.append(social_div)
+            } else if (member_list[i].socials[j].includes("twitch")) {
+                var social_div = document.createElement('div');
+                $(social_div).addClass("col")
+                var current_social = '<a class="bio-twitch p-1" href= ' + member_list[i].socials[j] + ' target="_blank"><svg class="bi" width="24" height="24"> <use xlink:href="#twitch" /> </svg></a>'
 
+                social_div.innerHTML = current_social;
+                footer_div.append(social_div)
             }
         }
+
+        card_div.append(footer_div)
 
         // Append Elements
         col_div.append(card_div)
